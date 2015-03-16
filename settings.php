5c0a773fb7f802817e7fadd565d09228ba6ab5b9
EldinSalkovic <eldinsalkoviic@gmail.com>
2015-03-16 15:58:08 +0100

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="412 Bayfront Place, Suite 104 Naples, Fl 34102."; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=2394039669   ;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
